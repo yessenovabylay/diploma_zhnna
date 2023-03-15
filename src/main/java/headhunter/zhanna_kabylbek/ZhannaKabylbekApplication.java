2acc87c06945a2e0df9e3d57b03994c75e220922
Abylay <yessenov.abylay@gmail.com>
2023-03-15 17:12:45 +0300

    first commit

---
+++ src/main/java/headhunter/zhanna_kabylbek/ZhannaKabylbekApplication.java
@@ -0,0 +1,13 @@
+package headhunter.zhanna_kabylbek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhannaKabylbekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhannaKabylbekApplication.class, args);
+	}
+
+}
